06b68833517874ef527a72c45895e3d7c90efcdd
Valentin <valentingaudin@gmail.com>
2023-03-10 18:02:54 +0100

    * 💄 style(NavBar.tsx): update app name in navbar The app name in the navbar has been updated to "Beautifully app" to improve the user experience and branding.

--- platform/src/components/NavBar.tsx
+++ platform/src/components/NavBar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
                 <div className="flex items-center justify-between h-16">
                     <div className="flex-shrink-0">
                         <a href="/" className="text-white">
-                            My app de TOTO
+                            Beautifully app
                         </a>
                     </div>
                     <div className="hidden md:block">
